cf27f49cf3e418233fa1e03361ddb0ba6c5eadae
joshneipp <joshneipp@gmail.com>
2015-10-22 15:14:00 -0400

    Configure Gemfile for heroku.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,3 @@
 class Member < ActiveRecord::Base
+  has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+member1 = Member.create!(username: "member1", password: "password")
+member2 = Member.create!(username: "member2", password: "password")
+member3 = Member.create!(username: "member3", password: "password")
+
+question1 = Question.create!(member_id: 1, title: "Question 1", question_body: "This is a question?", question_votes: 3)
+question2 = Question.create!(member_id: 1, title: "Question 2", question_body: "This is another question?", question_votes: 2)
+question3 = Question.create!(member_id: 2, title: "Question 3", question_body: "This is the third question?", question_votes: 1)
+
+answer1 = Answer.create!(member_id: 2, question_id: 1, answer_body: "Answer goes here.", answer_votes: 15, accepted: true)
+answer2 = Answer.create!(member_id: 3, question_id: 1, answer_body: "Answer goes here.", answer_votes: 4, accepted: false)
